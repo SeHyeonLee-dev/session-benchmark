3c83aa026e1084cff5684d02363a737ebe327ec6
SeHyeonLee-dev <sktpgus2023@gmail.com>
2025-04-27 23:03:14 +0900

    feat: User 엔티티 Serializable 적용

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -3,11 +3,13 @@ package com.example.demo.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import java.io.Serializable;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class User {
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
 	private String id;
 	private String username;
 }
